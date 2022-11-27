614b542d1c979f2a08899cb85c2388a06d490499
Gavin Eadie <gavin@umich.edu>
2022-11-27 13:18:28 -0500

    remove a difference marker ..

--- Sources/swift/swift/time.swift
+++ Sources/swift/swift/time.swift
@@ -60,11 +60,7 @@ public func dtgToUTC(_ DTG19: String?) -> Double {
     guard let timeString = DTG19 else {
         fatalError("dtgToUTC failure: NULL string")
     }
-<<<<<<< HEAD
 
-=======
-
->>>>>>> autogen
     guard let DTGToUTCPointer = dlsym(libHandle, "DTGToUTC") else {
         fatalError("dlsym failure: \(String(cString: dlerror()))")
     }
