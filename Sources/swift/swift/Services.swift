666aa862b2afb15ce2f9c0a0f096726711a5e7b8
Gavin Eadie <gavin@umich.edu>
2022-12-18 01:32:26 -0500

    add some useful services ..

--- Sources/swift/swift/Services.swift
+++ Sources/swift/swift/Services.swift
@@ -28,7 +28,7 @@ func getDylibPath() -> String {
     //    if let dylibDirectory = ProcessInfo.processInfo.environment["LD_LIBRARY_PATH"] {
     //        return dylibDirectory + "/"
     //    }
-    
+
     return "/usr/local/lib/sgp4prop/"
 }
 
@@ -41,19 +41,32 @@ public func freeAllDlls() {
 }
 
 extension String {
-    
+
     public func trimRight() -> String {
         String(reversed().drop { $0.isWhitespace }.reversed())
     }
+
+    func stringToTmpFile(_ fileName: String) -> String {
+
+        let urlForString = createFile(fileName, inURL: FileManager.default.temporaryDirectory)
+        do {
+            try self.write(to: urlForString, atomically: false, encoding: .ascii)
+        } catch {
+            print("stringToTmpFile: \(error.localizedDescription)")
+        }
+
+        return urlForString.path
+    }
+
 }
 
 func getFunctionPointer(_ libHandle: UnsafeMutableRawPointer?,
                         _ functionName: String) -> UnsafeMutableRawPointer {
-    
+
     guard let functionPointer = dlsym(libHandle, functionName) else {
         fatalError("dlsym failure: \(String(cString: dlerror()))")
     }
-    
+
     return functionPointer
 }
 
@@ -70,7 +83,7 @@ extension CChar {
 }
 
 extension RangeReplaceableCollection {
-    
+
     /// If not already at least the given length, appends enough copies of a
     /// given element to reach that length.
     public mutating func pad(toLength count: Int, with element: Element) {
@@ -78,24 +91,9 @@ extension RangeReplaceableCollection {
     }
 }
 
-extension StringProtocol {
-    var data: Data { .init(utf8) }
-    var bytes: [UInt8] { .init(utf8) }
-}
-
 // ----------------
 
-
-// convert "char line1[512]" to String
-
-// https://duan.ca/2020/09/09/free-c-strings/
-
-func c128_to_s(c: UnsafePointer<CUnsignedChar>) -> String {
-    String(cString: c)
-}
-
 // https://developer.apple.com/documentation/swift/string/init(cstring:)-2p84k
-
 
 public func makeCString(from str: String) -> UnsafeMutablePointer<Int8> {
     let count = str.utf8.count + 1
@@ -111,14 +109,14 @@ public func makeCString(from str: String) -> UnsafeMutablePointer<Int8> {
 extension Date {
     /// The number of seconds from 1 January 1950 to the reference date, 1 January 2001.
     public static let timeIntervalBetween1950AndReferenceDate: TimeInterval = 1_609_545_600.0   // 86,400 sec/day -> 18,629 days
-    
+
     /// Returns a `Date` initialized relative to 00:00:00 UTC on 1 January 1950 by a given number of seconds.
     public init(timeIntervalSince1950: TimeInterval) {
         self.init(timeIntervalSinceReferenceDate: timeIntervalSince1950 - Date.timeIntervalBetween1950AndReferenceDate)
     }
 }
 
-/// Converts a Swift `Date` type to days since 1950-01-01 (ds50UTC)
+/// Converts a Swift `Date` type to days since 1950-00-00 (ds50UTC)
 /// - Parameter date: a `Date`
 /// - Returns: days since 1950
 public func dateToUTC(_ date: Date) -> Double {
@@ -155,8 +153,6 @@ public func printWarning(_ softwareName: String) {
         """
     )
 }
-
-//TODO: MOVE TO SERVICES
 
 /// Obtains the URL of the directory created at the URL `inURL`
 /// - Parameters:
@@ -229,4 +225,17 @@ public func characterArrayToString(_ array: [Int8], size: Int32) -> String {
     _array[Int(size)] = 0
     return String(cString: _array).trimRight()
 
+}
+
+public func dllVersion() -> Double {
+
+    if #available(macOS 13.0, *) {
+        if let dllMainVersion = astroFuncGetInfo().firstMatch(of: #/\w(\d\.\d)/#) {
+            return Double(dllMainVersion.output.1) ?? 0.0
+        }
+    } else {
+        print("Can't do regex")
+    }
+
+    return 0.0
 }
